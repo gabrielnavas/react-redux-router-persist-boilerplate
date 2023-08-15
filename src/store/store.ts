daae29384adb5f65fae31d1900ebb1f71e259531
navas <gabrielnavas@email.com>
2023-08-14 23:34:47 -0300

    fix ignore actions on redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,32 +1,46 @@
-import { combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 import counterReducer from './features/counter/counterSlice'
 import userReducer from './features/user/userSlice'
 
 // add the reducers that need to be persisted
-const reducersToPersist: string[] = ['counter', 'user'];
+const reducersToPersist: string[] = ['counter', 'user']
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: reducersToPersist
-};
+}
 
 const reducers = combineReducers({
   counter: counterReducer,
   user: userReducer
-});
+})
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
   reducer: persistedReducer,
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
 
-const persistor = persistStore(store);
+const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
@@ -34,4 +48,4 @@ export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type StoreDispatch = typeof store.dispatch
 
-export { store, persistor };
+export { store, persistor }
